bcacaf184bdcd64b95289a61ececfc6136f60293
Konstantin Naryshkin <knary@users.noreply.github.com>
2017-10-19 22:48:10 -0400

    Try to get Python 3.7 working by adding setuptools_scm to build

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 setup(
     name = "Streams",
     version = "0.1",
-    setup_requires=['pytest-runner'],
+    setup_requires=['pytest-runner', 'setuptools_scm'],
     tests_require=['pytest', 'pytest-cov'],
+    use_scm_version=True,
     packages=['streams']
 )
