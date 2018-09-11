f1ce272d9370f85ff51ac7b18da95aece01ece41
Konstantin Naryshkin <knary@users.noreply.github.com>
2018-09-11 19:17:35 -0400

    Trying to fix pypy-5.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
     name = "Streams",
     version = "0.1",
-    setup_requires=['pytest-runner', 'sphinx'],
+    setup_requires=['pytest-runner', 'sphinx==1.7.0'],
     tests_require=['pytest', 'pytest-cov'],
     packages=['streams'],
     zip_safe=True
