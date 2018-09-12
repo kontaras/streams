55ea3a0df334cb5aeb24e4722dbcabdbd2585b56
Konstantin Naryshkin <knary@users.noreply.github.com>
2018-09-11 20:34:36 -0400

    Get support for setup.py with Python 2.6, 3.3, and 3.4

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
 from setuptools import setup, find_packages
+import sys
+
+_py_major, _py_minor, _py_release, _py_level, _py_serial = sys.version_info
+
+setup_reqs = ['pytest-runner']
+if (_py_major == 2 and _py_minor == 6) or (_py_major == 3 and _py_minor == 3):
+    setup_reqs.append('sphinx==1.4.9')
+elif _py_major == 3 and _py_minor == 2:
+    setup_reqs.append('sphinx==1.2.3')
+else:
+    setup_reqs.append('sphinx')
+
 setup(
     name = "Streams",
     version = "0.1",
-    setup_requires=['pytest-runner', 'sphinx==1.7.0b2'],
+    setup_requires=setup_reqs,
     tests_require=['pytest', 'pytest-cov'],
     packages=['streams'],
     zip_safe=True
