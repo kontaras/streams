4ff1e0311c3355af056e08cdd9cba2fc8346a213
Konstantin Tarashchanskiy <knary@users.noreply.github.com>
2019-03-05 18:38:21 -0500

    Try to fix Python 2.7 and 3.4 build

--- setup.py
+++ setup.py
@@ -9,10 +9,13 @@ if _py_major == 2 and _py_minor == 6:
 else:
     setup_reqs.append('pytest-runner')
 
-if (_py_major == 2 and _py_minor == 6) or (_py_major == 3 and _py_minor == 3):
-    setup_reqs.append('sphinx==1.4.9')
-elif _py_major == 3 and _py_minor == 2:
+
+if _py_major == 3 and _py_minor == 2:
     setup_reqs.append('sphinx==1.2.3')
+elif (_py_major == 2 and _py_minor == 6) or (_py_major == 3 and _py_minor == 3):
+    setup_reqs.append('sphinx==1.4.9')
+elif (_py_major == 2 and _py_minor == 7) or (_py_major == 3 and _py_minor == 4):
+    setup_reqs.append('sphinx==1.8.4')
 else:
     setup_reqs.append('sphinx')
 
