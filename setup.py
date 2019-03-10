5f83ba6f0623c93b08b9485fd5a401fe34d61182
Konstantin Tarashchanskiy <knary@users.noreply.github.com>
2019-03-10 18:26:47 -0400

    Try to fix PyPy by forcing old versions of libraries

--- setup.py
+++ setup.py
@@ -9,32 +9,14 @@ tests_reqs = []
 print("Python version")
 print(_py_major, _py_minor, _py_release, _py_level, _py_serial)
 
-if _py_major == 2 and _py_minor == 6:
-    setup_reqs.append('pytest=3.2.5')
-elif (_py_major == 2 and _py_minor == 7) or (_py_major == 3 and _py_minor == 4):
-    setup_reqs.append('pytest<=4.2.1')
-else:
-    setup_reqs.append('pytest>=3.6') #needed for pytest-cov
+setup_reqs.append('pytest==3.2.5')
 
-if _py_major == 2 and _py_minor == 6:
-    setup_reqs.append('pytest-runner==2.5.1')
-else:
-    setup_reqs.append('pytest-runner')
+setup_reqs.append('pytest-runner==2.5.1')
 
 
-if _py_major == 3 and _py_minor == 2:
-    setup_reqs.append('sphinx==1.2.3')
-elif (_py_major == 2 and _py_minor == 6) or (_py_major == 3 and _py_minor == 3):
-    setup_reqs.append('sphinx==1.4.9')
-elif (_py_major == 2 and _py_minor == 7) or (_py_major == 3 and _py_minor == 4):
-    setup_reqs.append('sphinx==1.8.4')
-else:
-    setup_reqs.append('sphinx')
+setup_reqs.append('sphinx==1.2.3')
 
-if _py_major == 3 and _py_minor == 4:
-    tests_reqs.append('pytest-cov==2.5.1')
-else:
-    tests_reqs.append('pytest-cov')
+tests_reqs.append('pytest-cov==2.5.1')
 
 setup(
     name = "Streams",
